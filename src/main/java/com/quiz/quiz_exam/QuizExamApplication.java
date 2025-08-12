9bddfce82656b5a3fd910c81d1a0c150514407f8
Gihan Jayasooriya <120779781+Gihanmadhusankha@users.noreply.github.com>
2025-08-12 11:52:41 +0530

    Initial commit

---
+++ src/main/java/com/quiz/quiz_exam/QuizExamApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.quiz_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quiz/quiz_exam/QuizExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.quiz_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
