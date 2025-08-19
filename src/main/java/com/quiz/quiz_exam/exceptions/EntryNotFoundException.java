6d2891c7a2290f9ff99d97b205538a5536ac1d27
Gihan Jayasooriya <120779781+Gihanmadhusankha@users.noreply.github.com>
2025-08-19 15:32:57 +0530

    jwt authentication

--- src/main/java/com/quiz/quiz_exam/exceptions/EntryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.quiz.quiz_exam.exceptions;
-
-public class EntryNotFoundException extends RuntimeException{
-    public EntryNotFoundException(String message){
-        super(message);
-    }
-}
